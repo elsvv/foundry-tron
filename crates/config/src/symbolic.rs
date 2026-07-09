26809c63884ef4122b11fb546a5326a9c6724457
Gustavo Figueiredo <me@figtracer.com>
2026-07-09 15:23:39 +0100

    chore(fuzz): temporarily default symbolic worker for benchmarks

--- crates/config/src/symbolic.rs
+++ crates/config/src/symbolic.rs
@@ -113,7 +113,7 @@ impl Default for SymbolicConfig {
         Self {
             enabled: false,
             seed_corpus: false,
-            fuzz_worker: false,
+            fuzz_worker: true,
             use_fuzz_corpus: false,
             corpus_seed_limit: 32,
             use_fuzz_frontiers: false,
